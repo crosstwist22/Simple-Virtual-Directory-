f028f808fb899a54aa2537648919a151c2d6d60d
Cross Twist <kotascross22@gmail.com>
2017-06-10 15:52:09 +0800

    Add files via upload

---
+++ View/View_Controller.php
@@ -0,0 +1,7 @@
+<?php
+namespace Ikigami\View\Controller;
+class View_Controller { 
+    function __construct() {
+        echo __CLASS__ . "<br>";
+    }
+}
---
+++ View/View_Modal.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Ikigami\View\Modal;
+class View_Modal {
+    function __construct() {
+        echo __CLASS__ . "<br>";
+    }
+}

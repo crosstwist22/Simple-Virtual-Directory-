c50d2024101d5a8c0a1ddee405a05d30c6383665
Cross Twist <kotascross22@gmail.com>
2017-06-10 15:48:43 +0800

    Modal Class

--- Modal/Modal.php
+++ Modal/Modal.php
@@ -1,1 +1,14 @@
+<?php
+
+namespace Ikigami\Modal;
+use Ikigami\Modal\Controller as Controller;
+use Ikigami\Modal\View as View;
+
+class Modal {
+    function __construct() {
+        new Controller\Modal_Controller();
+        new View\Modal_View();
+        echo __CLASS__ . "<br>";
+    }
+}
 

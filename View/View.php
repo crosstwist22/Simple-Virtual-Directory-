dc305978a11d4ec8f0e13859fd96d5c349f2dfe6
Cross Twist <kotascross22@gmail.com>
2017-06-10 15:51:47 +0800

    View Class

--- View/View.php
+++ View/View.php
@@ -1,1 +1,14 @@
+<?php
+
+namespace Ikigami\View;
+use Ikigami\View\Controller as Controller;
+use Ikigami\View\Modal as Modal;
+
+class View {
+    function __construct() {
+        new Controller\View_Controller();
+        new Modal\View_Modal();
+        echo __CLASS__ . "<br>";
+    }
+}
 

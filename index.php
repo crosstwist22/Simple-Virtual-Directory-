f395fb1b345a9d6a79067353bde46c7a1367e141
Cross Twist <kotascross22@gmail.com>
2017-06-10 15:46:57 +0800

    index

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+  $directory_content  = array_filter(glob('*'), 'is_dir');
+    foreach ($directory_content as $directory) {
+      if($directory === 'nbproject'){
+          continue;
+      }
+       $directory_content_files = array_diff(scandir($directory), array('..', '.'));
+      foreach ($directory_content_files as $directory_files){
+          include $directory . '/' . $directory_files; 
+          $directory_file = basename($directory_files,'.php');
+          echo $directory . '=>' . $directory_file . "<br>";
+      }
+    }
+    echo "<br>";
+    new Ikigami\Controller\Controller();
+    new Ikigami\Modal\Modal();
+    new Ikigami\View\View();
+?>

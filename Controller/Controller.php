bd1b73ff22f83ea1796187910c3b9f3f4f1437b3
Cross Twist <kotascross22@gmail.com>
2017-06-10 15:47:36 +0800

    Controller Class

--- Controller/Controller.php
+++ Controller/Controller.php
@@ -1,1 +1,13 @@
+<?php
 
+namespace Ikigami\Controller;
+use Ikigami\Controller\View as View;
+use Ikigami\Controller\Modal as Modal;
+
+class Controller {
+    function __construct() {
+        new View\Control_View();
+        new Modal\Control_Modal();
+        echo __CLASS__ . "<br>";
+    }
+}
